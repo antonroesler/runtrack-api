15b52bc7ae23f3c0ef2ae61268d0c4b92b5c8ecc
Anton Rösler <anton.roesler@stud.fra-uas.de>
2021-11-13 17:53:14 +0100

    HeartRate Data

--- api/models/runModel.js
+++ api/models/runModel.js
@@ -2,6 +2,18 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var HrSchema = new Schema({
+  avg: Number,
+  max: Number,
+  min: Number,
+  std: Number,
+  median: Number,
+  q1: Number,
+  q3: Number,
+  p1: Number,
+  p9: Number,
+  iqr: Number,
+});
 
 var RunSchema = new Schema({
   distance: Number,
@@ -19,6 +31,9 @@ var RunSchema = new Schema({
   flightsClimbed: Number,
   start: Date,
   end: Date,
+  hrData: HrSchema,
+  shoe: String
 });
 
+module.exports = mongoose.model('HR', HrSchema);
 module.exports = mongoose.model('Run', RunSchema);
